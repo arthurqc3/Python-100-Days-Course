60835860ebe159f3844016a68ccf4025a72a4e93
Arthur Quintela <86750553+arthurqc3@users.noreply.github.com>
2023-07-21 18:20:28 -0300

    Update caraOuCoroa.py

--- Day 1 - 14 Newbie/Day 4 - Pedra Papel Tesoura/caraOuCoroa.py
+++ Day 1 - 14 Newbie/Day 4 - Pedra Papel Tesoura/caraOuCoroa.py
@@ -1,7 +1,21 @@
 import random
 
-#Remember to use the random module
-#Hint: Remember to import the random module here at the top of the file. 🎲
-	 
-#Write the rest of your code below this line 👇
+def verificar(resposta):
+    numero = random.randint(0,1)
+    if numero == 1:
+        print("Deu Cara")
+    else:
+        print("Deu Coroa")
+    if resposta == numero:
+        print(f"Voce Ganhou")
+    else:
+        print(f"Voce Perdeu!")
 
+escolha = input("Escolha Cara ou Coroa:\n").title()
+
+if escolha == "Cara":
+    escolha = 1
+    verificar(escolha)
+elif escolha == "Coroa":
+    escolha = 0
+    verificar(escolha)
